bf0c58b4b12802d105afb8371816af36f3f7a39a
ddmgg00 <49672239+ddmgg00@users.noreply.github.com>
2024-04-10 16:01:13 +0800

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,12 +1,21 @@
+// pages/admin.js
+
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-export default function Admin() {
+const AdminPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace('/admin'); // 重定向到 /admin
+    // 当页面加载完成时，手动导航到 Netlify CMS 管理界面
+    router.push('/admin');
   }, []);
 
-  return null; // 这个页面不需要任何内容，直接进行重定向即可
-}
+  return (
+    <div>
+      {/* 在页面加载时可以显示加载中的提示，或者留空 */}
+    </div>
+  );
+};
+
+export default AdminPage;
